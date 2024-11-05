723a27230c27e6e76720f9fb0afe4bc0b622a60a
deutschich <140229250+deutschich@users.noreply.github.com>
2024-11-05 16:10:27 +0100

    Update WAUpdater.bat

--- WAUpdater.bat
+++ WAUpdater.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Welcome to WAUpdater
 echo This program updates many apps on the computer
-echo Version 2024/1 by deutschich
+echo Version 2024/2 by deutschich
 echo Check for updates...
 winget update
 echo If you want to install the updates, press any key!
